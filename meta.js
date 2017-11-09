07be578a3348fa35e2e73a7d691990de36cb2a35
Jerry Hsieh <jerryhsieh@Jerryde-MacBook-Pro-3.local>
2017-11-09 14:31:29 +0800

    change meta.js

--- meta.js
+++ meta.js
@@ -88,5 +88,6 @@ module.exports = {
     "test/e2e/**/*": "e2e",
     "src/router/**/*": "router"
   },
+  "skipInterpolation": ["src/**/*.vue", "node_modules/**"],
   "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
 };
